d08c3ee6641f321d8c8519fab95b085f733768da
PRAJIT54268 <prajitverma16@gmail.com>
2026-06-23 03:21:44 +0530

    update

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
-const { storeReturnTo } = require('../middleware');
+const { storeReturnTo } = require('../middlewares');
 
 module.exports.renderRegister = (req, res) => {
     res.render('users/register');
